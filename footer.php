a92fac50068701056d70f1ceb4cbbc3dea91e179
field2 <ben@field2.com>
2022-04-19 08:31:13 -0400

    dc

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
 <footer id="site_footer">
-	©<?php echo date('Y'). ' '.get_bloginfo('description'); ?>. All rights reserved.
+	©<?php echo date('Y'). ' '.get_bloginfo('description'); ?>. All rights reserved.<?php edit_post_link(); ?></div><!--  /.edit_post -->
+
 </footer><!-- /#site_footer  -->
--- header.php
+++ header.php
@@ -2,10 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<title><?php echo get_bloginfo('description').' :: '.get_the_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
-<body>
+<body <?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); ?>style="background-image:url(<?php echo $featured_img_url; ?>">
 	<header>
 		<?php 
 	if ( has_nav_menu( 'primary' ) ) {
