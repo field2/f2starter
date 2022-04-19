c6ba1c314acef3a44c046f0afabf253d0f519cb1
field2 <ben@field2.com>
2022-04-19 09:50:04 -0400

    working mobile nav

--- header.php
+++ header.php
@@ -9,17 +9,29 @@
 </head>
 <body <?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); ?>style="background-image:url(<?php echo $featured_img_url; ?>">
 	<header>
-		<?php 
-	if ( has_nav_menu( 'primary' ) ) {
-	wp_nav_menu(array('theme_location'=> 'primary', 'container'=>'nav', 'menu_class'=>'primary')); 
-}
-?>
-
-		<?php 
+<a class="home_link" href="<?php echo home_url(); ?>"><?php echo get_bloginfo('description'); ?></a>
+<div class="header_links">
+			<?php 
 	if ( has_nav_menu( 'social' ) ) {
 	wp_nav_menu(array('theme_location'=> 'social', 'container'=>'nav', 'menu_class'=>'social')); 
 }
 ?>
+	<nav class="site_links">
+<label for="menu-toggle" class="hamburger">
+<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="12" y="16" width="24" height="3" rx="1.5" fill="#f2f3f8"/>
+<rect x="12" y="24" width="24" height="3" rx="1.5" fill="#f2f3f8"/>
+<rect x="12" y="32" width="24" height="3" rx="1.5" fill="#f2f3f8"/>
+</svg>
+</label>
+<input type="checkbox" id="menu-toggle" class="visually-hidden"/>
+		<?php 
+	if ( has_nav_menu( 'primary' ) ) {
+	wp_nav_menu(array('theme_location'=> 'primary', 'container'=>'none', 'menu_class'=>'primary')); 
+}
+?>
+</nav><!--  /.site_links -->
 
-
+		
+</div><!--  /.header_links -->
 	</header>
